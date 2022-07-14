4ce73fe2ebab4b68b3758a67b5c2c53c2b3adea3
thazinmoe <thazinmoe1097@gmail.com>
2022-07-14 16:27:17 +0630

    second

--- index1.php
+++ index1.php
@@ -1,6 +1,8 @@
-<?php
-function add($a, ...$b) {
- print_r($b);
-}
-add(1, 2, 3, 4);
+<?php 
+phpinfo();
+//function add($a, ...$b) {
+// print_r($b);
+//}
+//add(1, 2, 3, 4);
 // Array ( [0] => 2 [1] => 3 [2] => 4 )
+?>
