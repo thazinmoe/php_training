ebdfc2d1119af7b0486536397ec418765a8c19e3
thazinmoe <thazinmoe1097@gmail.com>
2022-07-15 11:19:21 +0630

    tutorial1

--- tutorials/tutorial_01/index.php
+++ tutorials/tutorial_01/index.php
@@ -1,7 +1,31 @@
-<?php 
-function add($a, ...$b) {
- print_r($b);
-}
-add(1, 2, 3, 4);
-// Array ( [0] => 2 [1] => 3 [2] => 4 )
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial_01</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+    <table>
+        <?php
+        for ($rows = 1; $rows <= 8; $rows++) {
+            echo "<tr>";
+            for ($columns = 1; $columns <= 8; $columns++) {
+                if (($rows + $columns) % 2 == 0) {
+                    echo "<td class='black'></td>";
+                } else {
+                    echo "<td></td>";
+                }
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
